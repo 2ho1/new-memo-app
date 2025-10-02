dbd78f8620c2c3e59aef79ed7dab656555b991cb
2ho1 <lscv456@gmail.com>
2025-10-02 18:44:41 +0900

    Apply Neo-brutalism design system with custom colors and shadows

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import '../styles/globals.css'
 import { AuthProvider } from '../src/contexts/AuthContext'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+      </Head>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </>
   )
 }
