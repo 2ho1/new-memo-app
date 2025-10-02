d1c1a613ae3bd3a35855e80411ef150795952bf5
2ho1 <lscv456@gmail.com>
2025-10-02 18:14:43 +0900

    Fix import paths for Pages Router

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '../src/app/globals.css'
+import { AuthProvider } from '../src/contexts/AuthContext'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react'
-import { useAuth } from '../contexts/AuthContext'
-import { LoginForm } from '../components/LoginForm'
-import { RegisterForm } from '../components/RegisterForm'
-import { MemoList } from '../components/MemoList'
-import { MemoForm } from '../components/MemoForm'
-import { Header } from '../components/Header'
+import { useAuth } from '../src/contexts/AuthContext'
+import { LoginForm } from '../src/components/LoginForm'
+import { RegisterForm } from '../src/components/RegisterForm'
+import { MemoList } from '../src/components/MemoList'
+import { MemoForm } from '../src/components/MemoForm'
+import { Header } from '../src/components/Header'
 
 export default function Home() {
   const { user, loading } = useAuth()
